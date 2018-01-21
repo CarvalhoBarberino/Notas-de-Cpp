4201e7064c0a93263f5850d6d8f65ee0973934cd
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-01-20 23:35:24 -0200

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+//Titulo: Hello world!
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Hello world!" << endl;
+    cout << "Press enter to continue ...";
+    cin.get();
+    return 0;
+}
