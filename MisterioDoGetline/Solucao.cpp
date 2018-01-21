03731f38fb745bf82068fdc1face37315fe7fda9
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-01-21 03:07:30 -0200

    Create Solucao.cpp

---
+++ MisterioDoGetline/Solucao.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void carregaEstudante(int *numeroDeMatricula, string *nomeDoEstudante){
+    cout << "\nDigite o numero do numero de matricula do estudantes: ";
+    cin >> *numeroDeMatricula;
+    cin.ignore();//****Esta linha é responsavel por limpar o buffer, o que resolve o problema.****
+    cout << "\nDigite o nome do estudante: ";
+    getline(cin, *nomeDoEstudante);
+}
+void mostraEstudante(int *n, string nome){
+    cout << "numero de matricula do estudante: " << *n << endl;
+    cout << "Nome do estudante: " << nome << endl;
+}
+int main()
+{
+    int numero = 0;
+    string nome;
+    carregaEstudante(&numero, &nome);
+    mostraEstudante(&numero, nome);
+    return 0;
+}
