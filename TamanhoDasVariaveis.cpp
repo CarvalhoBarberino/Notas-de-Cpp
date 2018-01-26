85ae2e7eac00f5ff243cebd341faac1e564d4b5e
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-01-25 22:16:21 -0200

    Create TamanhoDasVariaveis.cpp

---
+++ TamanhoDasVariaveis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    bool a;
+    short b;
+    int c;
+    char d;
+    float e;
+    double f;
+    cout << "Tamanho das variaveis na memoria do computador" << endl;
+    cout << "bool --> " << sizeof(a) << endl;
+    cout << "short --> " << sizeof(b) << endl;
+    cout << "int --> " << sizeof(c) << endl;
+    cout << "char --> " << sizeof(d) << endl;
+    cout << "float --> " << sizeof(e) << endl;
+    cout << "double --> " << sizeof(f) << endl;
+}
