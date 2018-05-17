4b996a66c1833c84e60288bdf2ce1dde767e98c5
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-05-16 22:21:57 -0300

    Create Fatorial.cpp

---
+++ Fatorial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int fat(int i)
+{
+    if(i == 1)
+    {
+        return 1;
+    }
+    if(i == 0)
+    {
+        return 1;
+    }
+    return i * fat(i - 1);
+}
+int main()
+{
+    int i = 0;
+    cout << "Digite um numero inteiro:" << endl;
+    cin >> i;
+    i = fat(i);
+    cout << i << endl;
+    cin.ignore(1024, '\n');
+    cout << "Press enter to continue...";
+    cin.get();
+			 return 0;
+}
